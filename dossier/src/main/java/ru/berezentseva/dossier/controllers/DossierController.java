f7e30f4013b3b1ae0dd9c7f8f80cee14511dbdcb
Selectel Git User <ev.berezentseva@gmail.com>
2025-02-10 21:31:55 +0300

    + исправления под докер

--- dossier/src/main/java/ru/berezentseva/dossier/controllers/DossierController.java
+++ dossier/src/main/java/ru/berezentseva/dossier/controllers/DossierController.java
@@ -2,6 +2,7 @@ package ru.berezentseva.dossier.controllers;
 
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
--- dossier/src/main/java/ru/berezentseva/dossier/services/EmailMessageConsumerService.java
+++ dossier/src/main/java/ru/berezentseva/dossier/services/EmailMessageConsumerService.java
@@ -7,7 +7,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
